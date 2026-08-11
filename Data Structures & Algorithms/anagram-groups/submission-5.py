9395af8fb1ad0439376a24ebcb3d45a3df0af5c0
Nikita Hovyadinov <138826031+nikitahovy@users.noreply.github.com>
2026-08-10 20:00:13 -0400

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.py
@@ -0,0 +1,9 @@
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hash = defaultdict(list)
+        for word in strs:
+            temp = "".join(sorted(word))
+            hash[temp].append(word)
+        matrix = [value for value in hash.values()]
+        return matrix
